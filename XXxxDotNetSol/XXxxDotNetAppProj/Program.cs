a3c9ab31ef1d859b50e262b67e2576f236414ad5
orlorkor.simawa <orlorkor.simawa@outlook.com>
2024-01-28 22:16:31 -0500

    initial or first commit on local repository

---
+++ XXxxDotNetSol/XXxxDotNetAppProj/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// this is the prompt to enter your name
+Console.WriteLine(""); // creating white space
+Console.WriteLine("What is your name?");
+
+// variable studentName decalation and definition
+var studentName = Console.ReadLine();
+
+Console.WriteLine(""); // creating white space
+// this is the prompt to enter courseNum
+Console.WriteLine("What is this course number?");
+// variable studentName decalation and definition
+var CourseNum = Console.ReadLine();
+
+// Console.WriteLine() to display the content
+Console.WriteLine($"{Environment.NewLine} Hello, {studentName}!");
+Console.WriteLine($"{Environment.NewLine}You are welcome to, {CourseNum} \n");
